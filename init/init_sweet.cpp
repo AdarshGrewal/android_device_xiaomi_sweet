6e0191a08829b29e38c639a5a5873f5e458f3383
AdarshGrewal <adarsh@aosip.dev>
2021-05-13 16:21:24 +0530

    sweet: init: set fingerprint to a passing one

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -111,6 +111,10 @@ void vendor_load_properties() {
             }
         }
 
+    // SafetyNet workaround
+    fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
+    description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
+
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
